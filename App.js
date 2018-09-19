2f84c88cf046de4a3b5d4a38a0f77d5cebe25c69
Dezainer <gameboymasterz@gmail.com>
2018-09-19 12:30:39 -0300

    Initial setup done

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Immobile</Text>
+            </View>
+        )
+    }
+}
