a959cffca11ba275132752b49ad3c03ff682d0f4
Bruno Casali <brunoocasali@gmail.com>
2018-12-22 20:34:44 -0200

    Move functions from statistics component to newly statisticsService

---
+++ src/services/statisticsService.js
@@ -0,0 +1,25 @@
+import AsyncStorageHelper from '../helpers/asyncStorageHelper/asyncStorageHelper'
+import FormatHelper from '../helpers/formatHelper'
+
+let HOUR_PRICE = 2.12
+
+const watchAndSummarizeParking = callback => (
+	AsyncStorageHelper.subscribe('parking.onchange', parkingHistory => {
+    const totalTime = parkingHistory
+      .filter(parking => parking.leftAt != null)
+      .reduce((acc, cur) => acc + (cur.leftAt - cur.stoppedAt), 0)
+
+    const totalMoney = calculateTotalPrice(totalTime)
+
+    callback({
+      time: FormatHelper.formatTime(totalTime),
+      money: FormatHelper.formatMoney(totalMoney)
+    })
+	})
+)
+
+const calculateTotalPrice = milliseconds => (
+  ((milliseconds / 36000000) * HOUR_PRICE).toFixed(2)
+)
+
+export default { watchAndSummarizeParking }
