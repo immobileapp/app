929fc789a23ce371b7b2a1593b6da0e729ea27e2
Allan Horst <allanhorstwillig@gmail.com>
2018-12-28 21:47:54 -0200

    Licence plate mask

--- src/screens/login/components/licencePlate.js
+++ src/screens/login/components/licencePlate.js
@@ -2,13 +2,22 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import style from '../loginStyle'
 import genericStyle from '../../../genericStyle'
-import TextInput from '../../../components/input/textInput'
+import Masked from '../../../components/input/masked'
 import GenericStructure from './genericStructure'
 
 export default class LicencePlate extends React.Component {
 
     state = {
-        licence: ''
+        maskedValue: '',
+        unmaskedValue: ''
+    }
+
+    handleChange(maskedvalue, unmaskedValue) {
+        console.warn(maskedvalue)
+        this.setState({ 
+            maskedValue: maskedvalue, 
+            unmaskedValue: unmaskedValue
+        })
     }
 
     forward() {
@@ -23,9 +32,12 @@ export default class LicencePlate extends React.Component {
                     <Text style={ style.welcomeText } >Veículo</Text>
                     <Text style={ style.description }>Informe a placa do seu veículo</Text>
                     <View style={ style.inputWrapper}>
-                        <TextInput value={ this.state.licence }
+                        <Masked value={ this.state.maskedValue }
                             placeholder="AAA-1234"
-                            onChangeText={ licence => this.setState({ licence }) } />
+                            mask="[AAA]=[0000]"
+                            onChangeText={ 
+                                (maskedValue, unmaskedValue) => this.handleChange(maskedValue, unmaskedValue) 
+                            } />
 
                     </View>
                 </View>
