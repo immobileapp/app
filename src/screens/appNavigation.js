c6a12f7671832daf27988d52bd0d7511433d51c2
Allan Horst <allanhorstwillig@gmail.com>
2018-12-29 10:53:23 -0200

    Merged from master

--- src/screens/appNavigation.js
+++ src/screens/appNavigation.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import { createMaterialTopTabNavigator } from 'react-navigation'
+import { createAppContainer, createMaterialTopTabNavigator } from 'react-navigation'
 
-import Home from './home/home'
-import Parked from './parked/parked'
+import Parked from './screens/parked/parked'
+import Screens from './screens/screens'
 
-export default class AppNavigation extends React.Component {
+export default class Navigation extends React.Component {
 
-	getNavigator() {
-		return createMaterialTopTabNavigator({
-			'Home': { screen: Home },
-			'Parked': { screen: Parked }
-		}, {
-			swipeEnabled: true,
-			tabBarOptions: {
-				style: {
-					display: 'none'
-				}
-			}
-		})
-	}
+  getNavigator() {
+    return createAppContainer(createMaterialTopTabNavigator({
+    'Screens': {
+        screen: Screens
+      },
+      'Parked': { screen: Parked },
+      }, {
+        swipeEnabled: true,
+        tabBarOptions: {
+          style: {
+            display: 'none'
+          }
+        }
+      })
+    )
+  }
 
-	render() {
-		const Navigator = this.getNavigator()
-		return <Navigator />
-	}
+  render() {
+    const Navigator = this.getNavigator()
+    return <Navigator />
+  }
 }
