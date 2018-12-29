ab86c62cb69577e1d2a51de4e36d52d6c696044c
Allan Horst <allanhorstwillig@gmail.com>
2018-12-29 10:45:13 -0200

    Renamed to addCar

--- src/services/carService.js
+++ src/services/carService.js
@@ -1,8 +1,8 @@
 import AsyncStorageHelper from '../helpers/asyncStorageHelper/asyncStorageHelper'
 
-const saveCar = car => {
+const addCar = car => {
     car.model = 'VW Golf'
     AsyncStorageHelper.save('veichles', car)
 }
 
-export default { saveCar }
+export default { addCar }
