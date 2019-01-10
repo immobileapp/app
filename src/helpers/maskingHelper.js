a26ac7bf8a08fd061ca022a69040b30abc11fb58
Bruno Casali <brunoocasali@gmail.com>
2019-01-10 11:10:57 -0200

    Resolve identation

--- src/helpers/maskingHelper.js
+++ src/helpers/maskingHelper.js
@@ -1,30 +1,30 @@
 const mask = (value, pattern) => {
-	let unmasked = unmask(value),
-    	valueIndex = 0,
-        ended = false
-        
-	return pattern.split('').map(patternChar => {
-    	if(valueIndex >= unmasked.length || ended) return
-        let nextChar = unmasked.charAt(valueIndex)
-        
-    	if(areSameType(nextChar, patternChar)) {
-        	valueIndex++
-        	return nextChar
-        }
-        
-        if(valueIndex < unmasked.length && unmask(patternChar) === '')
-            return patternChar
+  let unmasked = unmask(value),
+      valueIndex = 0,
+      ended = false
 
-        ended = true
-    }).join('')
+  return pattern.split('').map(patternChar => {
+    if(valueIndex >= unmasked.length || ended) return
+      let nextChar = unmasked.charAt(valueIndex)
+
+    if(areSameType(nextChar, patternChar)) {
+      valueIndex++
+      return nextChar
+    }
+
+    if(valueIndex < unmasked.length && unmask(patternChar) === '')
+      return patternChar
+
+    ended = true
+  }).join('')
 }
 
 const unmask = value => (
-    value.replace(/[^\w\s]/gi, '')
+  value.replace(/[^\w\s]/gi, '')
 )
 
 const areSameType = (valueChar, patternChar) => (
-	(!isNaN(valueChar) && !isNaN(patternChar)) ||
+  (!isNaN(valueChar) && !isNaN(patternChar)) ||
     (valueChar.match(/[a-z]/i) && patternChar.match(/[a-z]/i))
 )
 
